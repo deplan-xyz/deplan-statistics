02392f67bb5ed5b803503905323435f70817f502
kirillgladkov <crosfire2011@gmail.com>
2024-06-15 12:35:20 +0300

    initialize app

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-full flex flex-col justify-center items-center gap-2'>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+
+    </div>
+  )
+}
+
+export default App
